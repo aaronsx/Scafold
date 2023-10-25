f70dbac7ab22d7e807296d09b2ea0c57a52ca43b
Aaron <senen.00@gmail.com>
2023-10-25 11:56:38 +0200

    Agregar archivos de proyecto.

---
+++ Scaffolding/Models/GestorBibliotecaPersonalContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Scaffolding.Models;
+
+public partial class GestorBibliotecaPersonalContext : DbContext
+{
+    public GestorBibliotecaPersonalContext()
+    {
+    }
+
+    public GestorBibliotecaPersonalContext(DbContextOptions<GestorBibliotecaPersonalContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tabla> Tablas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Name=ConnectionStrings:Connection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tabla>(entity =>
+        {
+            entity.HasKey(e => e.IdLibro);
+
+            entity.ToTable("Tabla", "gbp_almacen");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Scaffolding/Models/Tabla.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scaffolding.Models;
+
+public partial class Tabla
+{
+    public int IdLibro { get; set; }
+
+    public string Name { get; set; } = null!;
+}
